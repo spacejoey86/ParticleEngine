d2a7b747ccba08b350f411751ae437d2c9c59ff0
Iestyn <i.b.25ab360@gmail.com>
2023-02-07 22:29:20 +0000

    velocity is a thing now

--- Particles.py
+++ Particles.py
@@ -1,6 +1,15 @@
 class Particle:
-    def __init__(self, pos, col):
+    def __init__(self, pos, col, vel):
         self.pos = pos
         self.col = col
+        # vel has unit pixel/sec 
+        self.vel = vel
     def Draw(self, surface):
         surface.set_at(self.pos, self.col)
+    def update(self, deltaTime):
+        # update vel
+        force = Vector2(2, 1.5)
+        
+        self.vel += force * deltaTime
+        # update pos
+        self.pos += self.vel * deltaTime
